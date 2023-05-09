8637d942543752f597842d2d12a9088ede6ec53c
Devesh Yadav <ydevesh136@gmail.com>
2023-05-09 19:02:03 +0530

    completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,45 @@
-
+import "./app.css"
+import React, { useState } from "react";
 function App() {
+  const [textinput,setinput]=useState("");
+
+  const changetext=(e:React.ChangeEvent<HTMLInputElement>):void=>{
+    setinput(e.target.value);
+    (document.getElementById("output") as HTMLElement).innerText="";
+  }
+  
+  const mysubmit=async (e:React.MouseEvent<HTMLButtonElement>)=>{
+        e.preventDefault();
+
+        var fullurl=textinput;
+        const weburl=`http://localhost:5000/shrinkit`;
+        var data=JSON.stringify({fullurl})
+        let obj={
+            method:'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body:data
+        };
+        const res=await fetch(weburl,obj);
+        const finalurl:string=await res.text();
+        setinput("");
+        (document.getElementById("output") as HTMLElement).innerText=finalurl;
+        (document.getElementById("output") as HTMLElement).setAttribute("href",finalurl);
+
+        return;
+  }
+  
   return (
     <>
-     
+  
+        <div id="mytitletext">URL SHORTENER</div>
+        <form id="formbox">
+          <input type="text" id="textinput" placeholder="Enter URL" value={textinput} onInput={changetext} />
+          <button id="btn" onClick={mysubmit}>Shrink</button>
+        </form>
+        <a id="output" target="_blank"></a>
+  
     </>
   )
 }
---
+++ src/index.ts
@@ -0,0 +1,71 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser"
+import { config } from "dotenv";
+import { createshort ,getbyFullUrl,getByshortUrl} from "./db/dbSchema";
+import { authentication, random } from "./helper/helper";
+import cors from "cors"
+const app=express();
+
+app.use(bodyParser.json());
+config();
+app.use(cors({
+    credentials:true
+}))
+app.get("/:shorturl",async(req:express.Request,res:express.Response)=>{
+    try{
+        const {shorturl}=req.params;
+        const existing= await getByshortUrl(shorturl).catch((error)=>{
+            console.log("unable to fetch from server");
+        })
+        if(existing){
+            existing.clicks++;
+            existing.save();
+            res.status(200).redirect(existing.fullurl)
+            return;
+        }
+        res.status(404).send("wrong")
+
+    }
+    catch(error){
+        
+        return res.status(400).send("error occured at register process")
+    }
+})
+app.post("/shrinkit",async (req:express.Request,res:express.Response)=>{
+    try{
+        const {fullurl}=req.body;
+        if(!fullurl){
+            return res.send(400).send("invalid")
+        }
+        const existing= await getbyFullUrl(fullurl).catch((err)=>{
+            console.log("unable to fetch from server");
+        });
+        if(existing){    
+            await existing.save()
+            return res.status(200).send(`http://localhost:5000/${existing.shorturl}`)
+        }
+        const salt=random();  
+        const newcreated=await createshort({
+            fullurl,
+            shorturl:(authentication(salt,fullurl).slice(0,10))
+        })
+
+        return res.status(200).send(`http://localhost:5000/${newcreated.shorturl}`);
+
+    }
+    catch(error){
+        return res.status(400).send("error occured at register process")
+    }
+})  
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server is listening at http://localhost:${process.env.PORT}`);
+})
+
+const mongourl:string=`mongodb+srv://${process.env.USER_ID}:${process.env.USER_PASS}@urldb.wceiyu6.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+mongoose.Promise=Promise;
+mongoose.connect(mongourl);
+mongoose.connection.on("error",(error:Error)=>{
+    console.log("error occured at initial connection with database");
+})   
