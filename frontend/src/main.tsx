30d5dfca97695e4ae96dc75a109139366f8bc8e2
Devesh Yadav <ydevesh136@gmail.com>
2023-05-25 13:34:29 +0530

    loading screen added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { lazy } from 'react'
+import "./index.css"
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-
-
+import { Suspense } from 'react'
+import { ColorRing } from 'react-loader-spinner';
+const App = lazy(() => import("./App"));
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Suspense
+      fallback={
+        <div>
+          <ColorRing
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+          />
+        </div>}
+    >
+      <App />
+    </Suspense>
   </React.StrictMode>,
 )
